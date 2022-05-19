f4515a04bd746c41ec2676c1b05e2170a97ec2fc
Clarisa <clarisacandia0108@gmail.com>
2022-05-19 15:54:44 -0300

    Termine clase Nota y clase Punto

--- interaccionConElUsuario/trabajoPractico3/PruebaNota.java
+++ interaccionConElUsuario/trabajoPractico3/PruebaNota.java
@@ -3,8 +3,19 @@ package trabajoPractico3;
 public class PruebaNota {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Nota alumno1 = new Nota(3);
+		
+		System.out.println("La nota es: "+alumno1.getValor());
+		
+		System.out.println(alumno1.aprobado());
+		
+		Nota alumno2 = new Nota(7);
+		System.out.println(alumno2.aprobado());
+		
+		Nota alumno3 = new Nota(2);
+		alumno3.recuperar(8);
+		System.out.println(alumno3.aprobado());
 	}
+	
 
 }
